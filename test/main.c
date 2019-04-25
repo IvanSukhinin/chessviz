1faf3afdd71aa6725ac0a5300db362e95be245a6
ssshazam <ssssshazam@gmail.com>
2019-04-26 04:20:04 +0700

    Add figure move tests

--- test/main.c
+++ test/main.c
@@ -11,6 +11,15 @@
 #define CMD_NUM "1."
 #define CMD_CAPACITY 10
 
+char board[LINE][LINE] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                          {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                          {'.', '.', '.', '.', '.', '.', '.', '.'},
+                          {'.', '.', '.', '.', '.', '.', '.', '.'},
+                          {'.', '.', '.', '.', '.', '.', '.', '.'},
+                          {'.', '.', '.', '.', '.', '.', '.', '.'},
+                          {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                          {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+
 int main(int argc, const char** argv)
 {
     return ctest_main(argc, argv);
@@ -39,5 +48,64 @@ CTEST(input_test, check_numeration)
 
     // Then
     const int expected = 2;
+    ASSERT_EQUAL(expected, result);
+}
+
+// CHECK FIGURE MOVE
+CTEST(move_test, check_get_figure_type)
+{
+    // Given
+    char cmd[CMD_CAPACITY] = CMD_MOVE;
+
+    // When
+    const int result = getFigureType(board, cmd);
+
+    // Then
+    const int expected = 'P';
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(move_test, check_range)
+{
+    // Given
+    char cmd[CMD_CAPACITY] = CMD_MOVE;
+
+    // When
+    const int result = checkRange(cmd);
+
+    // Then
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(move_test, check_normal_level_move_errors)
+{
+    // Given
+    char cmd[CMD_CAPACITY] = CMD_MOVE;
+    locationCoordinates l;
+    l.figureLocationX = 6;
+    l.figureLocationY = 4;
+    destinationCoordinates d;
+    d.figureDestinationX = 4;
+    d.figureDestinationY = 4;
+
+    // When
+    const int result = checkMoveErrors(board, cmd, 'P', l, d);
+
+    // Then
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(move_test, check_figure_move)
+{
+    // Given
+    char cmd[CMD_CAPACITY] = CMD_MOVE;
+
+    // When
+    const int result = checkFigureMove(board, cmd);
+
+    // Then
+    const int expected = 0;
     ASSERT_EQUAL(expected, result);
 }
