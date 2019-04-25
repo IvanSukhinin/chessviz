ed2bd7108be430a16929d9df63dc25fc29aea93a
ssshazam <ssssshazam@gmail.com>
2019-04-26 03:48:10 +0700

    Check figure type test

--- test/main.c
+++ test/main.c
@@ -1,5 +1,28 @@
-int main()
-{
+#define CTEST_MAIN
+#define CTEST_COLOR_OK
 
-	return 0;
+#include <board.h>
+#include <print_board.h>
+#include <chess.h>
+#include <const.h>
+#include <ctest.h>
+
+#define CMD_MOVE "e2-e4"
+#define CMD_CAPACITY 10
+
+int main(int argc, const char** argv)
+{
+    return ctest_main(argc, argv);
+}
+
+CTEST(input_test, check_figure_type) {
+    // Given
+    char turn[CMD_CAPACITY] = CMD_MOVE;
+
+    // When
+    const int result = checkFigureType(turn);
+
+    // Then
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
 }
