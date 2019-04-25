f62814cd36c386357f652d647d60f78aa3e9feea
ssshazam <ssssshazam@gmail.com>
2019-04-26 04:01:40 +0700

    Input cmd tests

--- test/main.c
+++ test/main.c
@@ -2,27 +2,42 @@
 #define CTEST_COLOR_OK
 
 #include <board.h>
-#include <print_board.h>
 #include <chess.h>
 #include <const.h>
 #include <ctest.h>
+#include <print_board.h>
 
 #define CMD_MOVE "e2-e4"
+#define CMD_NUM "1."
 #define CMD_CAPACITY 10
 
 int main(int argc, const char** argv)
 {
     return ctest_main(argc, argv);
 }
-
-CTEST(input_test, check_figure_type) {
+// CHECK INPUT CMD
+CTEST(input_test, check_figure_type)
+{
     // Given
-    char turn[CMD_CAPACITY] = CMD_MOVE;
+    char cmd[CMD_CAPACITY] = CMD_MOVE;
 
     // When
-    const int result = checkFigureType(turn);
+    const int result = checkFigureType(cmd);
 
     // Then
     const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(input_test, check_numeration)
+{
+    // Given
+    char cmd[CMD_CAPACITY] = CMD_NUM;
+
+    // When
+    const int result = checkNumeration(cmd, 0);
+
+    // Then
+    const int expected = 2;
     ASSERT_EQUAL(expected, result);
 }
